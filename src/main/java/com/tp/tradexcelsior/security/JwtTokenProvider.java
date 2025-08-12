26c4b9d924e56a278c35d405d538b2bb52b3dfa6
harinisuresh28 <vsssharini@gmail.com>
2025-08-12 12:56:48 +0530

    Update JwtTokenProvider.java

--- src/main/java/com/tp/tradexcelsior/security/JwtTokenProvider.java
+++ src/main/java/com/tp/tradexcelsior/security/JwtTokenProvider.java
@@ -56,7 +56,7 @@ public class JwtTokenProvider {
         .verifyWith((SecretKey) key())
         .build()
         .parse(token);
-    return true;        //hellooobgfhgfhgfhgfhg
+    return true; 
 
   }
 }
